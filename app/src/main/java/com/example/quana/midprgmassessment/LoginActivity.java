0a0baad72bdef6107dd70e1f800308a7709d30d0
lchisolm1217 <laquanachisolm@ac.c4q.nyc>
2017-12-06 21:25:31 -0500

    added part01

---
+++ app/src/main/java/com/example/quana/midprgmassessment/LoginActivity.java
@@ -0,0 +1,9 @@
+package com.example.quana.midprgmassessment;
+
+/**
+ * Created by Quana on 12/6/17.
+ */
+
+public class LoginActivity {
+
+}
--- app/src/main/java/com/example/quana/midprgmassessment/LoopActivity.java
+++ app/src/main/java/com/example/quana/midprgmassessment/LoopActivity.java
@@ -1,7 +1,10 @@
 package com.example.quana.midprgmassessment;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LoopActivity extends AppCompatActivity {
 
@@ -9,5 +12,46 @@ public class LoopActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loop);
+
+        new MyTask().execute("my int parameter");
+    }
+
+    private class MyTask extends AsyncTask<Integer, Integer, Integer> {
+
+        @Override
+        protected void onPreExecute() {
+
+        }
+
+        @Override
+        protected int doInBackground(Integer[] value) {
+            for (int i = 0; i > 100,000; i++) {
+                publishProgress();
+            }
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer...progress) {
+            TextView textView = (TextView) findViewById(R.id.textView);
+            textView.setText("Loops completed: " + doInBackground());
+
+        }
+
+        @Override
+        protected void onPostExecute(Integer result) {
+            TextView textView = (TextView) findViewById(R.id.textView);
+            textView.setText("Loops completed: " + doInBackground());
+
+
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+
+            //super.onPostExecute(result);
+            //LoopActivity.
+            //.startActivity(new Intent(activity, LoginActivity.class))
+
+        }
     }
 }
+
+
